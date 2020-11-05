10f61b82a7a496b22c963362acd630ed6d7044a2
Konyicska Mark <konyicska_mark98@yahoo.com>
2020-11-05 21:22:45 +0200

    new laravel project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = \App\Models\Product::all();
+return view('viewproducts', ['allProducts' => $products]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('createproduct');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'description'=>'required',
+            'count' => 'required',
+            'price' => 'required'
+        ]);
+        Product::create($request->all());
+        return redirect()->back()->with('success','Creat cu succes.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'count' => 'required',
+            'price' => 'required'
+        ]);
+        $project->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/updateproduct.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+    <html lang="{{ app()->getLocale() }}">
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <title>View Products | Product Store</title>
+        <i-- Styles etc. -->
+    </head>
+
+    <body>
+        <div class="flex-center positian-ref full-height">
+            <div class="content">
+                <h1>Actualizeaza produs</h1>
+                <table class="table table-striped">
+                    <thead>
+                        <td>Name</td>
+                        <td>Description</td>
+                        <td>Count</td>
+                        <td>Price</td>
+                    </thead>
+                    <tbody>
+                        
+                        <tr>
+                            <td>{{ $product->name }}</td>
+                            <td class="inner-table">{{ $product->description }}</td>
+                            <td class="inner-table">{{ $product->count }}</td>
+                            <td class="inner-table">{{ $product->price }}</td>
+                        </tr>
+                       
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </body>
+
+    </html>
